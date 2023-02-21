6147497a16f09f9aac006c16e6c55dfe2bb297ea
LokeshC2 <47241840+LokeshC2@users.noreply.github.com>
2023-02-21 13:12:34 +0530

    rebase

---
+++ styles.js
@@ -0,0 +1,19 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  button: {
+    backgroundColor: 'skyblue',
+    borderRadius: 8,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 10,
+  },
+});
+
+export default styles;
