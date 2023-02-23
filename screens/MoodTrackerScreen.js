992a579dd336716824d4928df65ad9c2ab5e4cea
LokeshC2 <47241840+LokeshC2@users.noreply.github.com>
2023-02-23 21:38:22 +0530

    reset mood tracker

--- screens/MoodTrackerScreen.js
+++ screens/MoodTrackerScreen.js
@@ -4,25 +4,8 @@ import { View } from 'native-base';
 
 export default function MoodTrackerScreen({ navigation }) {
   return (
-    <View style={styles.container}>
+    <View flex={1}>
       {/* <MoodTracker /> */}
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
