80161e6446c7c96f7d23ea2d1cb7e1ee1514f2b6
Siddhi Rajesh Taori <siddhitaori42@gmail.com>
2023-03-20 16:17:19 +0530

    henlo

--- screens/MoodTrackerScreen.js
+++ screens/MoodTrackerScreen.js
@@ -1,11 +1,14 @@
-import { View, Text, Box, Image } from 'native-base';
+import { View, Text, Box, Image, Button } from 'native-base';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { useColors } from '../hooks/useColors';
 
 import bg1 from '../assets/images/bg1.png';
+import { Calendar } from 'react-native-calendars';
 
 export default function MoodTrackerScreen({ navigation }) {
   const colors = useColors()
+  const ddate = new Date();
+
   return (
     <SafeAreaView>
       <View>
@@ -13,8 +16,15 @@ export default function MoodTrackerScreen({ navigation }) {
           <View borderRadius={15} bg={colors.primary} m={5} mb={1}>
             <Image source={bg1} alt="background" />
           </View>
+          <Text ml={5} color={"gray.600"}>{ddate.toDateString()}</Text>
           <Text fontSize="3xl" ml={4} fontWeight="bold" color={colors.text}>Check In</Text>
+          <Text ml={5} mb={4} fontSize={15} color={"gray.600"}>Pause and Ponder your State of Mind</Text>
+          <Button width='90%' mx={"auto"} mb={4}>
+            <Text fontSize='lg' color='white'>What are you feeling?</Text>
+          </Button>
         </View>
+
+        <Calendar initialDate={(new Date()).toDateString()} />
       </View>
     </SafeAreaView>
   );
