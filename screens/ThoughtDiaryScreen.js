61f8dbca7ebe376f8319ce2168cc874012b261ca
Tejaswini-codes <tejusrakhonde@gmail.com>
2023-04-06 16:27:25 +0530

    thought diary

--- screens/ThoughtDiaryScreen.js
+++ screens/ThoughtDiaryScreen.js
@@ -1,42 +1,55 @@
-import { View, Text, Image, Container, Row, Column, Link, Button } from 'native-base'
-import React from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
-import leaf from "../assets/images/leaf.png";
+import { View, Text, Button } from "native-base";
+import { StyleSheet } from "react-native";
+import React from "react";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { SafeAreaView } from "react-native-safe-area-context";
+import ThoughtScreen from "./ThoughtScreen";
+import Navigation from "../navigation";
 
-const ThoughtDiaryScreen = () => {
-  return (
-    <SafeAreaView>
-      <View>
-        <Container alignItems={'center'} m="auto" mt={10} borderWidth={1}>
-          <Row>
-            <Column display={"flex"} justifyContent="center" marginBottom={3}>
-              <Image source={leaf} alt="leaf" size='lg' mt={4} />
-            </Column>
-          </Row>
-          <Row>
-            <Column display={"flex"} justifyContent="center" marginBottom={2}>
-              <Text textAlign={"center"}>Thought Diary</Text>
-            </Column>
-          </Row>
-          <Row>
-            <Column display={"flex"} justifyContent="center">
-              <Text textAlign={"center"} color="gray.700" marginBottom={4}>
-                An online, guided thought diary app to help you capture and review
-                your thoughts. Following CBT principles and best practises.
+export default function ThoughtDiaryScreen() {
+  const ThoughtDiaryScreen = () => {
+    const showThoughtRecord = () => {
+      setopen(true);
+    };
+
+    const hideThoughtRecord = () => {
+      setopen(false);
+    };
+    const navigation = useNavigation();
+    return (
+      <SafeAreaView>
+        <ThoughtScreen open={open} hideThoughtRecord={hideThoughtRecord}>
+          <View style={styles.container}>
+            <TouchableOpacity activeOpacity={0.95} style={styles.button} />
+            <Button onPress={() => navigation.navigate("ThoughtScreen")}>
+              <Text style={{ color: "white", fontSize: 20 }}>
+                Enter a Thought Record
               </Text>
-            </Column>
-          </Row>
-          <Row>
-            <Column display={"flex"} justifyContent="center">
-              <Link to="/newThought">
-                <Button bgColor={'green.800'}>Get Started</Button>
-              </Link>
-            </Column>
-          </Row>
-        </Container>
-      </View>
-    </SafeAreaView>
-  )
+            </Button>
+            <Button>
+              <Text style={{ color: "white", fontSize: 20 }}>
+                See earlier records
+              </Text>
+            </Button>
+          </View>
+        </ThoughtScreen>
+      </SafeAreaView>
+    );
+  };
 }
-
-export default ThoughtDiaryScreen
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 200,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  button: {
+    flex: 1,
+    fontSize: 35,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 80,
+    maxWidth: 350,
+  },
+});
---
+++ screens/ThoughtScreen.js
@@ -0,0 +1,122 @@
+import { StyleSheet, TextInput } from "react-native";
+import { Modal, View, Text, Button } from "native-base";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { faBorderNone } from "@fortawesome/free-solid-svg-icons";
+
+//import styles from "./styles/ThoughtScreen.css";
+const ThoughtRecord = ({ open, hideThoughtRecorder }) => {
+  const [text, onChangeText] = React.useState("");
+  const [number, onChangeNumber] = React.useState("");
+  const [isHovered, setIsHovered] = React.useState(false);
+
+  const ddate = new Date();
+
+  return (
+    <SafeAreaView>
+      <Modal isOpen={open} onClose={hideThoughtRecorder} size="xl">
+        <Modal.Content>
+          <Modal.CloseButton></Modal.CloseButton>
+          <Modal.Header>Thought Record</Modal.Header>
+          <Modal.Body>
+            <Text style={styles.text}>Date : {ddate.toDateString()}</Text>
+            {/* Day ,date ,Situation,Thoughts, Feelings, Alternative Thoughts, New feelings */}
+            <Text style={styles.text}>Situation</Text>
+            <TextInput
+              label="situation"
+              style={[styles.input, isHovered && styles.inputHover]}
+              onChangeText={onChangeText}
+              value={text}
+              multiline={true}
+              onFocus={() => setIsHovered(true)}
+              onBlur={() => setIsHovered(false)}
+              placeholder="What were you doing?
+               What led to the unpleasant emotion? 
+             What distressing physical sensations did you have?"
+            />
+            <Text style={styles.text}>Automatic Thoughts (ATs)</Text>
+            <TextInput
+              label="ATs"
+              style={styles.input}
+              onChangeText={onChangeText}
+              value={text}
+              multiline={true}
+              placeholder=" What thought/s or image/s went through your mind?
+              How much did you believe the thought at the time (0-100%)?"
+            />
+            <Text style={styles.text}>Emotions</Text>
+            <TextInput
+              style={styles.input}
+              onChangeText={onChangeText}
+              value={text}
+              multiline={true}
+              placeholder="What emotion/s did you feel at the time?
+             How intense was the emotion (0-100%)?"
+            />
+            <Text style={styles.text}>Alternative Thoughts</Text>
+            <TextInput
+              style={styles.input}
+              onChangeText={onChangeText}
+              value={text}
+              multiline={true}
+              placeholder="Which thinking styles did you engage in?
+ How much do you believe each response (0-100%)?"
+            />
+            <Text style={styles.text}>Outcome</Text>
+            <TextInput
+              style={styles.input}
+              onChangeText={onChangeText}
+              value={text}
+              multiline={true}
+              placeholder="How much do you now believe your ATs (0-100%)?
+             What emotion/s do you now feel? At what intensity?"
+            />
+            <Button>
+              <Text>Save</Text>
+            </Button>
+          </Modal.Body>
+        </Modal.Content>
+      </Modal>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 200,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 20,
+  },
+  input: {
+    height: 100,
+    lineHeight: 28,
+    borderWidth: 2,
+    borderColor: "transparent",
+    borderBottomColor: "#777",
+    paddingTop: 4,
+    paddingBottom: 4,
+    outline: "none",
+    color: "#0d0c22",
+  },
+  text: {
+    fontWeight: 10,
+    fontSize: 15,
+    marginTop: 10,
+    marginBottom: 4,
+  },
+  // .input {
+  //   height: 80;
+  //   line-height: 28px;
+  //   border: 2px solid transparent;
+  //   border-bottom-color: #777;
+  //   padding: 0.2rem 0;
+  //   outline: none;
+  //   background-color: transparent;
+  //   color: #0d0c22;
+  //   transition: 0.3s cubic-bezier(0.645, 0.045, 0.355, 1);
+  // }
+});
+
+export default ThoughtRecord;
